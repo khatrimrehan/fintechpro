a26599946236496e994cbf149dabbbbe6c3e9b03
RK <khatrimrehan@gmail.com>
2026-07-27 18:47:53 +0530

    hrhe

--- pages/dashbrd.js
+++ pages/dashbrd.js
@@ -28,9 +28,9 @@ addBtn.addEventListener('click', () => {
     overlay.style.position = "fixed"
     overlay.style.background = "rgba(0,0,0,.28)"
     overlay.style.backdropFilter = "blur(8px)"
-    
-    model.style.display= "block"
-    
+
+    model.style.display = "block"
+
 })
 
 
@@ -38,7 +38,56 @@ closeD.addEventListener('click', () => {
     overlay.style.position = "relative"
     overlay.style.background = "transparent"
     overlay.style.backdropFilter = "none"
-    
-    model.style.display= "none"
-    
+
+    model.style.display = "none"
+
 })
+
+const select = document.getElementById("category");
+
+console.log(select.value);
+
+select.addEventListener("change", () => {
+    console.log(select.value);
+});
+
+
+const selEct = document.getElementById("curr");
+const totalammountEL = document.querySelector('.totalamount')
+const currentammountEL = document.querySelector('.currentamount')
+const totalexpenseEL = document.querySelector('.totalexpense')
+
+
+const applyBtn = document.querySelector('#applybtn')
+
+
+
+selEct.addEventListener("change", () => {
+
+    applyBtn.addEventListener('click', () => {
+
+
+
+
+
+        const currSym = selEct.value;
+
+
+
+        totalammountEL.innerHTML = `
+         <h1>${currSym}</h1>
+                                       `
+        
+        currentammountEL.innerHTML = `
+         <h1>${currSym}</h1>
+                                       `
+
+        totalexpenseEL.innerHTML = `
+         <h1>${currSym}</h1>
+                                       `
+    });
+
+
+})
+
+
